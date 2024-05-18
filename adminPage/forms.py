d8f8288f0bd522fabbe892a63426492cac967cdf
Mikigb77 <miquelgarcia.alu@gmail.com>
2024-05-18 12:23:36 +0200

    update

--- adminPage/forms.py
+++ adminPage/forms.py
@@ -6,6 +6,12 @@ from django import forms
 
 
 class UserForm(forms.ModelForm):
+    """
+    A form for updating user information in the admin page.
+
+    Args:
+        forms (ModelForm): A form for updating a User model instance.
+    """
     class Meta:
         model = User
         fields = '__all__'
